707437c070ad59a835721c58f0c5f7553e2e15d7
vinvin129 <59414444+vinvin129@users.noreply.github.com>
2021-05-04 18:15:44 +0200

    listeners javadoc

--- src/main/java/fr/vincatif/perso/bookManager/listeners/LibraryLoadListener.java
+++ src/main/java/fr/vincatif/perso/bookManager/listeners/LibraryLoadListener.java
@@ -2,7 +2,14 @@ package fr.vincatif.perso.bookManager.listeners;
 
 import java.util.EventListener;
 
+/**
+ * The load listener event interface. When the load event occurs, that object's loaded method is invoked.
+ * @author vincent GUILLOU
+ */
 @FunctionalInterface
 public interface LibraryLoadListener extends EventListener {
+    /**
+     * invoked when library is loaded from file
+     */
     void loaded();
 }
--- src/main/java/fr/vincatif/perso/bookManager/listeners/TextFieldListener.java
+++ src/main/java/fr/vincatif/perso/bookManager/listeners/TextFieldListener.java
@@ -4,15 +4,34 @@ import fr.vincatif.perso.bookManager.controllers.LibrarySearch;
 
 import java.util.EventListener;
 
+/**
+ * The update {@link java.awt.TextField} listener event interface. When the update, submit, or change event occurs, that object's update method is invoked.
+ * @author vincent GUILLOU
+ */
 @FunctionalInterface
 public interface TextFieldListener extends EventListener {
-
+    /**
+     * invoked on any change with an result of {@link LibrarySearch}
+     * @param result the {@link LibrarySearch} of result search
+     */
     void update(LibrarySearch result);
 
+    /**
+     * invoked on submit depending on the context with an result of {@link LibrarySearch}.<BR>
+     *     by default this method invoke update
+     * @param result the {@link LibrarySearch} of result search
+     * @see TextFieldListener#update(LibrarySearch)
+     */
     default void submit(LibrarySearch result) {
         update(result);
     }
 
+    /**
+     * invoked on character change with an result of {@link LibrarySearch}.<BR>
+     *     By default this method invoke update
+     * @param result the {@link LibrarySearch} of result search
+     * @see TextFieldListener#update(LibrarySearch)
+     */
     default void change(LibrarySearch result) {
         update(result);
     }
